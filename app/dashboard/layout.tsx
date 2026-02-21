b000f08039b3d1cc9c653ddd1ccf9d2bbce81097
redenetycomp-stack <redenetycomp@gmail.com>
2026-02-21 12:11:47 -0300

    Create layout.tsx

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
